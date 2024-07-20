9dbc4cd51e7f08db7f1ca4d06ad206e68f8fa829
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2024-07-19 19:07:14 -0600

    InfinityXStream

--- JavaScript/funcioneswhatsapp.js
+++ JavaScript/funcioneswhatsapp.js
@@ -3,7 +3,7 @@ function mandarMensaje() {
   
     // Resto del código...
     var numeroTelefono = '522225402887';
-    var mensaje = encodeURIComponent('Hola, estoy interesado en cotizar. ¿Cómo puedo empezar?');
+    var mensaje = encodeURIComponent('Hola, estoy interesado en cotizar un servicio de streaming. ¿Cómo puedo empezar?');
     var urlWhatsApp = 'https://wa.me/' + numeroTelefono + '?text=' + mensaje;
     window.location.href = urlWhatsApp;
   }
